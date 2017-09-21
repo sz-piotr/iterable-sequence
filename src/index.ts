b26b0ef689d842abce13fe8f8f54cc0f36c5c7c0
sz-piotr <szlachciak.piotr@gmail.com>
2017-09-21 20:43:13 +0200

    umd configuration

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,6 @@
-export default 'hello'
+const add: (a: number, b: number) => number
+    = (a, b) => a + b
+
+const result = add(1, 2)
+
+export default result
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,25 @@
 module.exports = {
-  entry: "./src/index.ts",
+  entry: './src/index.ts',
   output: {
-    filename: "bundle.js",
-    path: __dirname + "/lib"
+    filename: 'tswebpack.js',
+    path: __dirname + '/lib',
+    library: 'tswebpack',
+    libraryTarget: 'umd',
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   resolve: {
     extensions: [
-      ".ts",
-      ".tsx",
-      ".js",
-      ".json"
+      '.ts',
+      '.tsx',
+      '.js',
+      '.json'
     ]
   },
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: "awesome-typescript-loader"
+        loader: 'awesome-typescript-loader'
       }
     ]
   }
